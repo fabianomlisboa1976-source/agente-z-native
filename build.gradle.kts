b435e699aaeb136cdd421896fa29e7c34f1712b9
Z User <z@container>
2026-04-01 08:31:29 +0000

    Fix: Remove repos from build.gradle.kts (now in settings.gradle.kts)
    - Remove allprojects block that was causing FAIL_ON_PROJECT_REPOS error
    - Repositories are now correctly defined only in settings.gradle.kts
    - This fixes the Gradle build error on GitHub Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,9 @@
-// Top-level build file
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+// Note: Repositories are now defined in settings.gradle.kts
+// This is the modern Gradle approach (Gradle 7.0+)
